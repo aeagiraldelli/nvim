d1ae5c99103384f3995592266234e57bd2aa69f7
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-05-27 21:23:03 -0300

    feat: add colorschemes.

--- lua/plugins/catppuccin_colorscheme.lua
+++ lua/plugins/catppuccin_colorscheme.lua
@@ -1,9 +1,7 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
   },
 }
