c68eefcdd76dfb925797de39aa3eb18342277236
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-05-28 21:42:35 -0300

    update colorscheme plugins.

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("catppuccin")
+require("tokyonight").setup({
+  transparent = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+  style = "night",
+})
 require("lualine").setup({
   options = {
-    theme = "catppuccin",
+    theme = "tokyonight",
   },
 })
