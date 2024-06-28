b259367e5233f06c60bea20f255759a6459a38da
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-06-28 15:35:30 -0300

    clean up

--- init.lua
+++ init.lua
@@ -6,18 +6,11 @@ require("tokyonight").setup({
   styles = {
     comments = { italic = true },
     keywords = { italic = true },
-    functions = {},
-    variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
+    sidebars = "transparent",
+    floats = "transparent",
   },
   on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
 })
 
@@ -26,66 +19,11 @@ require("rose-pine").setup({
   dark_variant = "main", -- main, moon, or dawn
   dim_inactive_windows = false,
   extend_background_behind_borders = true,
-
   enable = {
     terminal = true,
     legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
     migrations = true, -- Handle deprecated options automatically
   },
-
-  styles = {
-    bold = true,
-    italic = true,
-    transparency = true,
-  },
-
-  groups = {
-    border = "muted",
-    link = "iris",
-    panel = "surface",
-
-    error = "love",
-    hint = "iris",
-    info = "foam",
-    note = "pine",
-    todo = "rose",
-    warn = "gold",
-
-    git_add = "foam",
-    git_change = "rose",
-    git_delete = "love",
-    git_dirty = "rose",
-    git_ignore = "muted",
-    git_merge = "iris",
-    git_rename = "pine",
-    git_stage = "iris",
-    git_text = "rose",
-    git_untracked = "subtle",
-
-    h1 = "iris",
-    h2 = "foam",
-    h3 = "rose",
-    h4 = "gold",
-    h5 = "pine",
-    h6 = "foam",
-  },
-
-  highlight_groups = {
-    -- Comment = { fg = "foam" },
-    -- VertSplit = { fg = "muted", bg = "muted" },
-  },
-
-  before_highlight = function(group, highlight, palette)
-    -- Disable all undercurls
-    -- if highlight.undercurl then
-    --     highlight.undercurl = false
-    -- end
-    --
-    -- Change palette colour
-    -- if highlight.fg == palette.pine then
-    --     highlight.fg = palette.foam
-    -- end
-  end,
 })
 
 require("nightfox").setup({
@@ -99,23 +37,12 @@ require("eldritch").setup({
   styles = {
     comments = { italic = true },
     keywords = { italic = true },
-    functions = {},
-    variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
+    sidebars = "transparent",
+    floats = "transparent",
   },
   lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
   on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
 })
 
