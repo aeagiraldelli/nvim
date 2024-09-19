891ccdacbac34f40ed6c35d17d2a5789c77b2074
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-09-19 20:25:47 -0300

    add typescript tools plugin.
    https://github.com/pmizio/typescript-tools.nvim

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.theme_config")
+require("config.ts_tools")
 vim.cmd("colorscheme tokyonight")
 vim.opt.pumblend = 0 -- disable transparency for cmp menu
 vim.opt.winblend = 0 -- disable transparency for documentation popup
