12d297af37970f5b370134c946c6ea975030c428
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-10-29 12:52:07 -0300

    add github colorscheme.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,6 +5,12 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "tjdevries/colorbuddy.vim" },
   {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
     "AlexvZyl/nordic.nvim",
     lazy = false,
     priority = 1000,
