c57315b09a423387e55de9a39604da2a95d5a0f9
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-11-12 10:55:26 -0300

    update default theme.

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ require("config.lazy")
 require("config.base")
 require("config.theme_config")
 require("config.ts_tools")
-vim.cmd("colorscheme github_dark_default")
+vim.cmd("colorscheme catppuccin")
 vim.opt.pumblend = 0 -- disable transparency for cmp menu
 vim.opt.winblend = 0 -- disable transparency for documentation popup
