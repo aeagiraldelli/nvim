a3d281aa396d804820cd9ccd089773b1eba3e360
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-11-29 13:43:22 -0300

    make lualine transparent with tokyonight theme.

--- init.lua
+++ init.lua
@@ -4,6 +4,11 @@ require("config.base")
 require("config.theme_config")
 require("config.ts_tools")
 
+-- Make lualine transparent with tokyonight theme.
+local ll_theme = require("lualine.themes.tokyonight")
+ll_theme.normal.c.bg = "None"
+require("lualine").setup({ options = { theme = ll_theme } })
+
 vim.cmd("colorscheme tokyonight")
 vim.opt.pumblend = 0 -- disable transparency for cmp menu
 vim.opt.winblend = 0 -- disable transparency for documentation popup
